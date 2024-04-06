418f3a1ed92dacd4d0dc6a04d6155b3b36a41cb1
LitSKt <150106361+LitSKt@users.noreply.github.com>
2024-04-06 14:30:21 +0800

    Add files via upload

--- Config/fish/config.fish
+++ Config/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
   set fish_greeting
-  oh-my-posh init fish --config $HOME/.poshthemes/catppuccin_mocha.omp.json | source
+  oh-my-posh init fish --config $HOME/.config/fish/themes/catppuccin_mocha.omp.json | source
 end
