4ebaf30825ae42270675e9348ee7c5936dedd35e
LitSKt <150106361+LitSKt@users.noreply.github.com>
2024-04-08 22:13:07 +0800

    Delete Config directory

--- Config/fish/config.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-  # Commands to run in interactive sessions can go here
-  set fish_greeting
-  starship init fish | source
-end
--- Config/nvim/lua/vim-options.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd("set nu")
-vim.cmd("set clipboard+=unnamedplus")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
