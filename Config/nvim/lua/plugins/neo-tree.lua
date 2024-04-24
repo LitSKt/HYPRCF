d2e04164b4ea6051be8d4ce8fff62738164f6b7c
LitSKt <150106361+LitSKt@users.noreply.github.com>
2024-04-24 16:00:10 +0800

    Add files via upload

--- Config/nvim/lua/plugins/neo-tree.lua
+++ Config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,15 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    vim.fn.sign_define("DiagnosticSignError",
+      { text = " ", texthl = "DiagnosticSignError" })
+    vim.fn.sign_define("DiagnosticSignWarn",
+      { text = " ", texthl = "DiagnosticSignWarn" })
+    vim.fn.sign_define("DiagnosticSignInfo",
+      { text = " ", texthl = "DiagnosticSignInfo" })
+    vim.fn.sign_define("DiagnosticSignHint",
+      { text = "󰌵", texthl = "DiagnosticSignHint" })
+
     vim.keymap.set('n', '<C-s>', ':Neotree filesystem reveal left<CR>')
     require("neo-tree").setup({
       popup_border_style = "double",
@@ -19,7 +28,7 @@ return {
       },
       window = {
         width = 28,
-      }
+      },
     })
   end
 }
