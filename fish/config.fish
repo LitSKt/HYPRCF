e9a57eaabdf0291e08ee7311f11c532404bf3ccb
LitSKt <150106361+LitSKt@users.noreply.github.com>
2024-04-25 17:02:57 +0800

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  set fish_greeting
+  starship init fish | source
+
+  export VISUAL="nvim"
+  export EDITOR="nvim"
+end
